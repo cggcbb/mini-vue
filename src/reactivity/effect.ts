0382610ab1ca4fe642850ee877ced4bc7b3dcec6
cggcbb <449567533@qq.com>
2023-09-23 21:46:59 +0800

    feat: effect runner

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -6,7 +6,7 @@ class ReactiveEffect {
   }
   run() {
     activeEffect = this;
-    this._fn();
+    return this._fn();
   }
 }
 
@@ -42,5 +42,10 @@ export function effect(fn: Function) {
   // 抽象出一个类
   const _effect = new ReactiveEffect(fn);
 
-  _effect.run();
+  // 修改函数this问题
+  const run = _effect.run.bind(_effect);
+
+  run();
+
+  return run;
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -18,4 +18,21 @@ describe('effect', () => {
     user.age++;
     expect(rAge).toBe(12);
   });
+
+  it('effect runner', () => {
+    // effect(fn) -> runner -> fn -> return
+    let foo = 10;
+    const returnRunner = 'runner';
+    // effect return runner
+    const runner = effect(() => {
+      foo++;
+      return returnRunner;
+    });
+
+    expect(foo).toBe(11);
+    // runner执行会再次调用fn, 并 return fn 返回值
+    const r = runner();
+    expect(foo).toBe(12);
+    expect(r).toBe(returnRunner);
+  });
 });
